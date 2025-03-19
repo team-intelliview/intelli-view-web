ccfd7dd22e6c3b7bbff89503b8d015c62c1a0994
Jeong-Ag <kjungu1105@gmail.com>
2025-03-19 20:06:25 +0900

    chore: add `Next.js`

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,33 @@
-import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 
-
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-];
+module.exports = {
+  files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:prettier/recommended",
+    "next/core-web-vitals",
+    "plugin:react/recommended",
+  ],
+  ignorePatterns: ["dist", "coverage", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  rules: {
+    "react/no-unused-state": "warn",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/prop-types": "off",
+  },
+  plugins: [
+    pluginJs,
+    tseslint,
+    pluginReact,
+  ],
+};
