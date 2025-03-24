95804f54909a75c8356bbd5adda8e614f1354f12
Jeong-Ag <kjungu1105@gmail.com>
2025-03-24 22:50:25 +0900

    feat: create `dropdown` component

---
+++ src/components/Dropdown.tsx
@@ -0,0 +1,45 @@
+import { cn } from '@/utils/string';
+import { useState } from 'react';
+import Image from 'next/image';
+
+interface DropdownProps {
+  title: string;
+  children: React.ReactNode;
+  className?: string;
+}
+
+export default function Dropdown({
+  title,
+  children,
+  className,
+}: DropdownProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div
+      className={cn(
+        'border-gray-20 items-start rounded-[16px] border bg-white p-[32px] break-keep hover:cursor-pointer',
+        className,
+      )}
+      onClick={() => setIsOpen(!isOpen)}
+    >
+      <div className="flex justify-between">
+        <p className="text-body1 text-gray-90 font-semibold">{title}</p>
+        <Image
+          width={24}
+          height={24}
+          src={isOpen ? '/icons/arrow_up.svg' : '/icons/arrow_bottom.svg'}
+          alt="arrow"
+        />
+      </div>
+      <div
+        className={cn(
+          'mt-[32px] leading-loose transition-all duration-300 ease-in-out',
+          isOpen ? 'opacity-100' : 'absolute h-0 overflow-hidden opacity-0',
+        )}
+      >
+        {children}
+      </div>
+    </div>
+  );
+}
