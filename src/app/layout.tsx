6748e98d3c3d22265f5aa53d27a553d3f308149a
Jeong-Ag <kjungu1105@gmail.com>
2025-03-24 23:05:03 +0900

    feat: set page layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+
+import './globals.css';
+
+export const metadata: Metadata = {
+  title: 'intelli-view',
+  description: '2025 경기대학교 컴퓨터공학전공 심화캡스톤 프로젝트',
+  icons: {
+    icon: '/favicon.ico',
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className="scrollbar-hide flex min-h-screen flex-col">
+        {children}
+      </body>
+    </html>
+  );
+}
