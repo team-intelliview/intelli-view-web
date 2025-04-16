57ccfd5d510327bcc580a3d6c1c2d75d0fdd3180
Jeong-Ag <kjungu1105@gmail.com>
2025-04-16 20:25:10 +0900

    feat: add routing for resume modification and export sections

--- src/app/home/sections/InfoSection.tsx
+++ src/app/home/sections/InfoSection.tsx
@@ -1,17 +1,28 @@
 'use client';
 
+import { PATH } from '@/constants';
 import { useUserState } from '@/hooks/useUser';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function InfoSection() {
+  const router = useRouter();
+
   const { name } = useUserState();
+
+  const handleMyResumeClick = () => {
+    router.push(PATH.MODIFY_RESUME);
+  };
 
   return (
     <div className="flex items-center justify-between py-[48px]">
       <p className="text-title2 text-gray-90 font-semibold">
         👋 {name}님, 안녕하세요
       </p>
-      <button className="flex gap-[4px] hover:cursor-pointer">
+      <button
+        className="flex gap-[4px] hover:cursor-pointer"
+        onClick={handleMyResumeClick}
+      >
         <p className="text-gray-90 text-heading2 font-semibold">내 이력서</p>
         <Image
           width={24}
