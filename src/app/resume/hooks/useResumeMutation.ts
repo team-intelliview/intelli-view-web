4f139a2bbe6e00fed18aceb79ecb7ebee0b83aca
Jeong-Ag <kjungu1105@gmail.com>
2025-04-27 21:36:48 +0900

    feat: 이력서 작성 및 수정 API 연동

---
+++ src/app/resume/hooks/useResumeMutation.ts
@@ -0,0 +1,26 @@
+import { postResume } from '@/api/resume';
+import { STATUS } from '@/constants/api';
+import { useMutation } from '@tanstack/react-query';
+
+interface Props {
+  handleNext:()=>void;
+}
+
+export const useResumeMutation = ({handleNext}:Props) => {
+  const mutation = useMutation({
+    mutationFn: postResume,
+    onSuccess: ({ status, message, data }) => {
+      if (status === STATUS.OK) {
+        handleNext()
+      } else {
+        throw new Error(message);
+      }
+    },
+  });
+
+  return {
+    resumeMutate: mutation.mutate,
+    isPending: mutation.isPending,
+    isSuccess: mutation.isSuccess,
+  };
+};
