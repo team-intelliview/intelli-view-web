8e554bd23afdd080b5381052ecfab6a202683fa7
Jeong-Ag <kjungu1105@gmail.com>
2025-05-30 02:12:20 +0900

    chore: svgr 세팅

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  },
   // 외부 이미지 도메인 허용
   images: {
     domains: ['lh3.googleusercontent.com', 'img1.kakaocdn.net'],
