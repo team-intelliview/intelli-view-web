989a5a6fb107d8a1315114c00fe5794bdd3c94b3
Jeong-Ag <kjungu1105@gmail.com>
2025-05-30 09:24:58 +0900

    feat: 면접 로그 조회 API 연동 및 자기소개서 첨삭 로그 연동 타입 수정

--- src/api/home.ts
+++ src/api/home.ts
@@ -1,5 +1,10 @@
 import { createURL, END_POINTS, STATUS } from '@/constants/api';
-import type { CoverLetterList, Pagination, PaginationParams } from '@/types';
+import type {
+  LogListItem,
+  InterviewItem,
+  Pagination,
+  PaginationParams,
+} from '@/types';
 import { addSearchParams, fetchRequest } from '@/utils';
 
 export const getCoverLetters = async ({
@@ -10,7 +15,47 @@ export const getCoverLetters = async ({
   const url = createURL(END_POINTS.COVER_LETTERS);
 
   const { status, message, data } = await fetchRequest<
-    Pagination<CoverLetterList>
+    Pagination<LogListItem>
+  >({
+    url: addSearchParams(url, params),
+  });
+
+  if (status !== STATUS.OK) {
+    throw new Error(message);
+  }
+
+  return { contents: data.contents };
+};
+
+export const getInterviews = async ({
+  page = 0,
+  size = 99,
+}: PaginationParams = {}) => {
+  const params = { page, size };
+  const url = createURL(END_POINTS.INTERVIEW);
+
+  const { status, message, data } = await fetchRequest<
+    Pagination<LogListItem>
+  >({
+    url: addSearchParams(url, params),
+  });
+
+  if (status !== STATUS.OK) {
+    throw new Error(message);
+  }
+
+  return { contents: data.contents };
+};
+
+export const getInterviewFeedback = async ({
+  page = 0,
+  size = 99,
+}: PaginationParams = {}) => {
+  const params = { page, size };
+  const url = createURL(END_POINTS.INTERVIEW_LIST);
+
+  const { status, message, data } = await fetchRequest<
+    Pagination<InterviewItem>
   >({
     url: addSearchParams(url, params),
   });
