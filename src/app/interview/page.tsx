987bca34223c57260f3cf61319aaa7863f427742
Jeong-Ag <kjungu1105@gmail.com>
2025-05-31 14:06:14 +0900

    refactor: 인터뷰 페이지 구조 개선 및 질문 데이터 로딩 방식 변경

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,66 +1,48 @@
 'use client';
 
 import PageLayout from '../PageLayout';
-import QuestionBoxSection from './sections/QuestionBoxSection';
-import SideSection from './sections/SideSection';
 import Content from '@/components/Content';
-import { useModalState } from '@/hooks';
-import { MODAL } from '@/constants';
-import CompleteInterviewModal from './components/CompleteInterviewModal';
-import QuitInterviewModal from './components/QuitInterviewModal';
-import VideoSection from './sections/VideoSection';
-import { useEffect, useState } from 'react';
-import { QuestionItem } from '@/types/question';
-import { getInterviewsQuestion } from '@/api/interview';
-import Loading from './loading';
+import { useEffect, Suspense, lazy } from 'react';
 import { useInterview } from '@/hooks/useInterview';
+import Loading from '../loading';
+import { useInterviewQuestions } from './hooks/useInterviewQuestion';
 
-export default function Interview() {
-  const [questionList, setQuestionList] = useState<QuestionItem[]>([]);
+const QuestionBoxSection = lazy(() => import('./sections/QuestionBoxSection'));
+const VideoSection = lazy(() => import('./sections/VideoSection'));
+const SideSection = lazy(() => import('./sections/SideSection'));
+
+export const Interview = () => {
+  const { data, isLoading } = useInterviewQuestions();
   const { resetInterview } = useInterview();
 
   useEffect(() => {
     resetInterview();
-
-    const fetchQuestions = async () => {
-      try {
-        const { contents } = await getInterviewsQuestion();
-        setQuestionList(contents);
-      } catch (error) {
-        console.error('API 요청 실패:', error);
-      }
-    };
-
-    fetchQuestions();
   }, []);
 
-  const { isOpen: isCompleteModalOpen } = useModalState({
-    key: MODAL.COMPLETE_INTERVIEW,
-  });
-  const { isOpen: isQuitInterviewModalOpen } = useModalState({
-    key: MODAL.QUIT_INTERVIEW,
-  });
-
-  if (questionList.length === 0) {
-    return <Loading />;
-  }
+  if (isLoading) return <Loading />;
 
   return (
-    <>
-      <PageLayout
-        interviewNav
-        className="flex h-fit w-screen justify-center overflow-hidden px-[10%]"
-      >
-        <Content className="my-[92px] h-fit w-full justify-center gap-[20px]">
-          <div className="flex flex-col gap-[20px]">
-            <QuestionBoxSection questionList={questionList} />
-            <VideoSection />
-          </div>
-          <SideSection questionList={questionList} />
-        </Content>
-      </PageLayout>
-      {isQuitInterviewModalOpen && <QuitInterviewModal />}
-      {isCompleteModalOpen && <CompleteInterviewModal />}
-    </>
+    <PageLayout
+      interviewNav
+      className="flex h-fit w-screen justify-center overflow-hidden px-[10%]"
+    >
+      <Content className="my-[92px] h-fit w-full justify-center gap-[20px]">
+        <div className="flex flex-col gap-[20px]">
+          <Suspense fallback={<Loading />}>
+            <QuestionBoxSection questionList={data} />
+          </Suspense>
+
+          <Suspense fallback={<Loading />}>
+            <VideoSection questionCnt={data.length} />
+          </Suspense>
+        </div>
+
+        <Suspense fallback={<Loading />}>
+          <SideSection questionList={data} />
+        </Suspense>
+      </Content>
+    </PageLayout>
   );
-}
+};
+
+export default Interview;
