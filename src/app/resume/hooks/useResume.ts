47dbfda555d218f86626d9cdd8f523a886d6d039
Jeong-Ag <kjungu1105@gmail.com>
2025-06-03 00:17:22 +0900

    refactor: 데이터 패칭 로직 개선

---
+++ src/app/resume/hooks/useResume.ts
@@ -0,0 +1,12 @@
+import { getResume } from '@/api/resume';
+import { QUERY_KEYS } from '@/constants/api';
+import { useSuspenseQuery } from '@tanstack/react-query';
+
+const useResume = () => {
+  return useSuspenseQuery({
+    queryKey: [QUERY_KEYS.RESUME],
+    queryFn: getResume,
+  });
+};
+
+export default useResume;
