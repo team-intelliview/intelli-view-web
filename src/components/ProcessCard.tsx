db382f63f38cc54c50e83acac6a10a0dffc97e74
Jeong-Ag <kjungu1105@gmail.com>
2025-06-02 18:27:24 +0900

    refactor: useLogList 훅 수정

--- src/components/ProcessCard.tsx
+++ src/components/ProcessCard.tsx
@@ -4,14 +4,7 @@ import { cn } from '@/utils/string';
 import dayjs from 'dayjs';
 import Image from 'next/image';
 
-interface ProcessCardProps extends CoverLetterList {
-  labelClassName?: string;
-  image?: string;
-  clickMore?: () => void;
-  className?: string;
-}
-
-export default function ProcessCard({
+const ProcessCard = ({
   id,
   title,
   status,
@@ -19,11 +12,8 @@ export default function ProcessCard({
   totalQuestion,
   completeQuestion,
   updatedAt,
-  labelClassName,
-  clickMore,
   image,
-  className,
-}: ProcessCardProps) {
+}: LogListItem) => {
   return (
     <div className="relative h-fit" id={`process-card-${id}`}>
       <div
@@ -34,12 +24,7 @@ export default function ProcessCard({
             : 'bg-gray-60',
         )}
       />
-      <div
-        className={cn(
-          'shadow1 border-light flex items-start gap-[24px] rounded-r-[20px] border p-[20px]',
-          className,
-        )}
-      >
+      <div className="shadow1 border-light flex items-start gap-[24px] rounded-r-[20px] border p-[20px]">
         {image && (
           <Image
             src={image}
@@ -50,56 +35,76 @@ export default function ProcessCard({
           />
         )}
         <div className="flex w-full flex-col gap-[8px]">
-          <div className="flex w-full justify-between">
-            <div
-              className={cn(
-                'text-body1 rounded-[30px] border px-[8px] py-[4px]',
-                status === PROGRESSING_STATUS.IN_PROGRESS
-                  ? 'text-blue bg-primary-40 border-gray-20'
-                  : 'bg-gray-10 border-gray-30 text-gray-80',
-                labelClassName,
-              )}
-            >
-              {status === PROGRESSING_STATUS.IN_PROGRESS ? '진행 중' : '완료'}
-            </div>
-            <button
-              className="border-gray-20 flex h-[28px] w-[28px] justify-center rounded-[4px] border bg-white hover:cursor-pointer"
-              onClick={clickMore}
-            >
-              <Image src="/icons/dot.svg" alt="more" width={20} height={20} />
-            </button>
-          </div>
+          <StatusLabel status={status} />
           <p className="text-headline1 font-semibold text-gray-100">{title}</p>
-          <div className="gird-col-3 grid gap-[4px]">
-            {status === PROGRESSING_STATUS.IN_PROGRESS ? (
-              <>
-                <p className="text-body1 font-regular text-gray-80">
-                  진행률<span className="ml-[8px]">{progress}%</span>
-                </p>
-                <p className="text-body1 font-regular text-gray-80">
-                  질문
-                  <span className="ml-[8px]">
-                    {completeQuestion}/{totalQuestion}
-                  </span>
-                </p>
-                <p className="text-body1 font-regular text-gray-80">
-                  응답일
-                  <span className="ml-[8px]">
-                    {dayjs(updatedAt).format('YYYY.MM.DD')}
-                  </span>
-                </p>
-              </>
-            ) : (
-              <p className="text-body1 font-regular text-gray-80">
-                제출일
-                <span className="ml-[8px]">
-                  {dayjs(updatedAt).format('YYYY.MM.DD')}
-                </span>
-              </p>
-            )}
-          </div>
+          {status === REQUEST_STATUS.IN_PROGRESS ? (
+            <InProgressDetailInfo
+              progress={progress}
+              completeQuestion={completeQuestion}
+              totalQuestion={totalQuestion}
+              updatedAt={updatedAt}
+            />
+          ) : (
+            <CompletedInfo updatedAt={updatedAt} />
+          )}
         </div>
       </div>
     </div>
   );
-}
+};
+
+const StatusLabel = ({ status }: Pick<LogListItem, 'status'>) => {
+  return (
+    <div
+      className={cn(
+        'text-body1 w-fit rounded-[30px] border px-[8px] py-[4px]',
+        status === REQUEST_STATUS.IN_PROGRESS
+          ? 'text-blue bg-primary-40 border-gray-20'
+          : 'bg-gray-10 border-gray-30 text-gray-80',
+      )}
+    >
+      {status === REQUEST_STATUS.IN_PROGRESS ? '진행 중' : '완료'}
+    </div>
+  );
+};
+
+const InProgressDetailInfo = ({
+  progress,
+  completeQuestion,
+  totalQuestion,
+  updatedAt,
+}: Pick<
+  LogListItem,
+  'progress' | 'completeQuestion' | 'totalQuestion' | 'updatedAt'
+>) => {
+  return (
+    <div className="gird-col-3 grid gap-[4px]">
+      <p className="text-body1 font-regular text-gray-80">
+        진행률<span className="ml-[8px]">{progress}%</span>
+      </p>
+      <p className="text-body1 font-regular text-gray-80">
+        질문
+        <span className="ml-[8px]">
+          {completeQuestion}/{totalQuestion}
+        </span>
+      </p>
+      <p className="text-body1 font-regular text-gray-80">
+        응답일
+        <span className="ml-[8px]">
+          {dayjs(updatedAt).format('YYYY.MM.DD')}
+        </span>
+      </p>
+    </div>
+  );
+};
+
+const CompletedInfo = ({ updatedAt }: Pick<LogListItem, 'updatedAt'>) => {
+  return (
+    <p className="text-body1 font-regular text-gray-80">
+      제출일
+      <span className="ml-[8px]">{dayjs(updatedAt).format('YYYY.MM.DD')}</span>
+    </p>
+  );
+};
+
+export default ProcessCard;
