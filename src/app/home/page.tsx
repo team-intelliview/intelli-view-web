ce8a450f4a1da0c65db94a012626729cf067e2af
Jeong-Ag <kjungu1105@gmail.com>
2025-06-02 18:24:56 +0900

    refactor: home 레이아웃 조정

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,15 +10,18 @@ const LogSection = dynamic(() => import('./sections/LogSection'));
 export default function Home() {
   return (
     <Content className="flex-col items-center">
-      <div className="bg-primary-40 flex h-2/5 w-screen" />
+      <div className="bg-primary-40 flex h-2/5 w-full" />
       <div className="bg-gray-10 flex h-full w-full" />
-      <div className="absolute mb-[64px] h-full pt-[62px]">
+      <div className="absolute mb-[64px] flex h-full w-3/4 flex-col items-center pt-[62px]">
         <Suspense fallback={<Loading />}>
           <InfoSection />
         </Suspense>
-        <Suspense fallback={<Loading />}>
-          <LogSection />
-        </Suspense>
+        <div className="flex h-[75%] w-full gap-[24px]">
+          <Suspense fallback={<Loading />}>
+            <LogSection type={REQUEST_OPTION.INTERVIEW} />
+            <LogSection type={REQUEST_OPTION.COVER_LETTER} />
+          </Suspense>
+        </div>
       </div>
     </Content>
   );
