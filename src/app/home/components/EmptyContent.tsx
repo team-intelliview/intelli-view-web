1288e6a2fda9c58caa1108a80d7b3065e21eecc4
Jeong-Ag <kjungu1105@gmail.com>
2025-06-02 18:28:41 +0900

    refactor: EmptyContent 컴포넌트 아이콘 svgr 방식으로 변경

--- src/app/home/components/EmptyContent.tsx
+++ src/app/home/components/EmptyContent.tsx
@@ -2,21 +2,27 @@ import { REQUEST_OPTION } from '@/constants';
 import { useContent } from '@/hooks';
 import { RequestOption } from '@/types';
 import { cn } from '@/utils';
-import Image from 'next/image';
+import MessageBlink from '@/assets/icons/message_blink.svg';
+import PaperBlink from '@/assets/icons/paper_blink.svg';
+import Plus from '@/assets/icons/plus.svg';
 
-export const EmptyContent = ({ type }: { type: RequestOption }) => {
+const EmptyContent = ({ type }: { type: RequestOption }) => {
   const { changeType } = useContent();
 
   const iconImage =
-    type === REQUEST_OPTION.INTERVIEW
-      ? '/icons/message_blink.svg'
-      : '/icons/paper_blink.svg';
+    type === REQUEST_OPTION.INTERVIEW ? (
+      <MessageBlink width={48} height={48} />
+    ) : (
+      <PaperBlink width={48} height={48} />
+    );
+
   const emptyMessage =
     type === REQUEST_OPTION.INTERVIEW
       ? `진행된 면접이 없습니다.
   모의 면접을 진행해보세요.`
       : `작성된 자기소개서가 없습니다.
   자기소개를 작성해보세요.`;
+
   const buttonText =
     type === REQUEST_OPTION.INTERVIEW ? '면접 진행하기' : '작성 진행하기';
 
@@ -26,16 +32,18 @@ export const EmptyContent = ({ type }: { type: RequestOption }) => {
         'py-auto flex h-full flex-col place-content-center items-center gap-[20px] text-center',
       )}
     >
-      <Image src={iconImage} alt="blink" width={48} height={48} />
+      {iconImage}
       <p className="text-headline1 whitespace-pre-wrap">{emptyMessage}</p>
       <button
         type="button"
         className="flex gap-4 px-[12px] py-[6px] hover:cursor-pointer"
         onClick={() => changeType(type)}
       >
-        <Image src="/icons/plus.svg" alt="plus" width={20} height={20} />
+        <Plus width={20} height={20} />
         <p>{buttonText}</p>
       </button>
     </div>
   );
 };
+
+export default EmptyContent;
