09fdf6a86d5dbfebeb5332d3344d920183a0ffcb
Jeong-Ag <kjungu1105@gmail.com>
2025-06-11 14:21:09 +0900

    refactor: home Suspense 제거

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,9 +1,7 @@
 export const dynamic = 'force-dynamic';
 
 import Content from '@/components/Content';
-import { Suspense } from 'react';
 import { InfoSection, LogSection } from './sections';
-import Loading from '@/components/Loading';
 import { REQUEST_OPTION } from '@/constants';
 
 export default async function Home() {
@@ -12,14 +10,10 @@ export default async function Home() {
       <div className="bg-primary-40 flex h-2/5 w-full" />
       <div className="bg-gray-10 flex h-full w-full" />
       <div className="absolute mb-[64px] flex h-full w-3/4 flex-col items-center pt-[62px]">
-        <Suspense fallback={<Loading />}>
-          <InfoSection />
-        </Suspense>
+        <InfoSection />
         <div className="flex h-[75%] w-full gap-[24px]">
-          <Suspense fallback={<Loading />}>
-            <LogSection type={REQUEST_OPTION.INTERVIEW} />
-            <LogSection type={REQUEST_OPTION.COVER_LETTER} />
-          </Suspense>
+          <LogSection type={REQUEST_OPTION.INTERVIEW} />
+          <LogSection type={REQUEST_OPTION.COVER_LETTER} />
         </div>
       </div>
     </Content>
